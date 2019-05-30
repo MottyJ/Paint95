3e3f93f9e67edf22fa41be476b1f5580afd98b50
MottyJ <mottyjaffe@gmail.com>
2019-05-30 22:57:53 +0300

    added some messy inline javascript, the ability to clear the canvas, 4 default colors, the painting ability, and the ability to erase

---
+++ js/paint95.js
@@ -0,0 +1,30 @@
+// var Canvas = {};
+
+// Canvas.start = function () {
+//     Canvas.bindMenuActions();
+// }
+
+// Canvas.bindMenuActions = function () {
+//     var newBtn = document.getElementById("new");
+//     newBtn.addEventListener("click", Canvas.new);
+//     var saveBtn = document.getElementById("save");
+//     saveBtn.addEventListener("click", Canvas.save);
+//     var loadBtn = document.getElementById("load");
+//     loadBtn.addEventListener("click", Canvas.load);
+// }
+
+// Canvas.new = function () {
+//     alert("new");
+// };
+// Canvas.save = function () {
+//     alert("save");
+// };
+// Canvas.load = function () {
+//     alert("load");
+// };
+
+// Canvas.start();
+
+// Canvas.shapes = []
+
+// Canvas.new
